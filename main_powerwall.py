f62aeeeef4b2dfebd2ed7186f088e05e58e7cdf6
Jakob Harlan <jakob.harlan@uni-weimar.de>
2014-09-03 13:58:48 +0200

    cleaned main_powerwall-py

--- main_powerwall.py
+++ main_powerwall.py
@@ -17,91 +17,6 @@ from examples_common.GuaVE import GuaVE
 ## Parameters:
 size = avango.gua.Vec2ui(2560/2, 2560*9/16)
 
-class TimedRotate(avango.script.Script):
-  TimeIn = avango.SFFloat()
-  MatrixOut = avango.gua.SFMatrix4()
-
-  @field_has_changed(TimeIn)
-  def update(self):
-    # self.MatrixOut.value = avango.gua.make_rot_mat(self.TimeIn.value*2.0, 0.0, 0.0, 1.0)
-    self.MatrixOut.value = avango.gua.make_rot_mat(90, 0.0, 0.0, 1.0)
-
-def setup_scene(graph):
-
-  pass
-  # root_node = avango.gua.nodes.TransformNode(
-  #   Name = "Root",
-  #   Transform =  avango.gua.make_trans_mat(0, 1.1, -2) * avango.gua.make_scale_mat(0.2)
-  # )
-
-  # group_monkey = avango.gua.nodes.TransformNode(
-  #   Name = "group_monkey",
-  # )
-
-  # group_light = avango.gua.nodes.TransformNode(
-  #   Name = "group_light",
-  # )
-
-  # group_light_spheres = avango.gua.nodes.TransformNode(
-  #   Name = "group_light_spheres",
-  # )
-
-  # group_light_2 = avango.gua.nodes.TransformNode(
-  #   Name = "group_light_2",
-  # )
-
-  # graph.Root.value.Children.value.append(root_node)
-  # root_node.Children.value.append(group_monkey)
-  # root_node.Children.value.append(group_light)
-  # root_node.Children.value.append(group_light_spheres)
-  # group_light.Children.value.append(group_light_2)
-
-  # # make a buitifull scene
-  # monkeys = []
-  # lights = []
-  # lights_spheres = []
-  # for i in range(16):
-  #   monkeys.append(
-  #                   loader.create_geometry_from_file(
-  #                   "ape" + str(i),
-  #                   "data/objects/monkey.obj",
-  #                   "data/materials/Stones.gmd",
-  #                   avango.gua.LoaderFlags.DEFAULTS)
-  #                 )
-
-  #   lights.append(
-  #                   avango.gua.nodes.PointLightNode(
-  #                   Name = "light" + str(i),
-  #                   Color = avango.gua.Color(1, 1, 1))
-  #                 )
-
-  #   lights_spheres.append(
-  #                         loader.create_geometry_from_file(
-  #                         "sphere" + str(i),
-  #                         "data/objects/light_sphere.obj",
-  #                         "data/materials/White.gmd",
-  #                         avango.gua.LoaderFlags.DEFAULTS)
-  #   )
-
-
-  # x = (len(monkeys) / 2) - len(monkeys)
-  # for i in range(len(monkeys)):
-  #   monkeys[i].Transform.value = avango.gua.make_trans_mat(x, 0, 0)
-  #   lights[i].Transform.value = avango.gua.make_trans_mat(x, 2, 0) * avango.gua.make_scale_mat(2)
-  #   lights_spheres[i].Transform.value = avango.gua.make_trans_mat(x, 2, 0) * avango.gua.make_scale_mat(0.15)
-  #   x += 1
-
-  # for monkey in monkeys:
-  #   group_monkey.Children.value.append(monkey)
-
-  # for light in lights:
-  #   group_light.Children.value.append(light)
-  #   group_light_2.Children.value.append(light)
-
-  # for lights_sphere in lights_spheres:
-  #   group_light_spheres.Children.value.append(lights_sphere)
-
-
 def viewing_setup_scene(graph):
 
   screen = avango.gua.nodes.ScreenNode(
@@ -151,44 +66,15 @@ def viewing_setup_scene(graph):
 
   return pipe
 
-
-def get_rotation_between_vectors(VEC1, VEC2):
-
-  VEC1.normalize()
-  VEC2.normalize()
-
-  _angle = math.degrees(math.acos(VEC1.dot(VEC2)))
-  _axis = VEC1.cross(VEC2)
-
-  return avango.gua.make_rot_mat(_angle, _axis)
-
-def calc_transform_connection(START_VEC, END_VEC):
-
-  # calc the vector in between the two points, the resulting center of the line segment and the scaling needed to connect both points
-  _vec = avango.gua.Vec3( END_VEC.x - START_VEC.x, END_VEC.y - START_VEC.y, END_VEC.z - START_VEC.z)
-  _center = START_VEC + (_vec * 0.5)
-  _scale  = _vec.length()
-
-  # calc the rotation according negative z-axis
-  _rotation_mat = get_rotation_between_vectors(avango.gua.Vec3(0, 0, -1), _vec)
-
-  # build the complete matrix
-  return avango.gua.make_trans_mat(_center) * _rotation_mat * avango.gua.make_scale_mat(0.1, 0.1, _scale)
-
-
 def start():
-
 
   ##initializing scene -------------------
   graph = avango.gua.nodes.SceneGraph(
     Name = "scenegraph"
   )
 
-  setup_scene(graph)
-
   loader = avango.gua.nodes.TriMeshLoader()
   Scene = avango.gua.nodes.TransformNode( Name = "Scene" )
-  rotate_node = avango.gua.nodes.TransformNode( Name = "rotate_node" )
 
   frame = loader.create_geometry_from_file(
     "frame"
@@ -196,7 +82,7 @@ def start():
     ,"data/materials/Hans.gmd"
     ,avango.gua.LoaderFlags.LOAD_MATERIALS | avango.gua.LoaderFlags.MAKE_PICKABLE
   )
-  rotate_node.Children.value.append(frame)
+  Scene.Children.value.append(frame)
 
   front = loader.create_geometry_from_file(
     "front"
@@ -204,7 +90,7 @@ def start():
     ,"data/materials/Hans.gmd"
     ,avango.gua.LoaderFlags.LOAD_MATERIALS | avango.gua.LoaderFlags.MAKE_PICKABLE
   )
-  rotate_node.Children.value.append(front)
+  Scene.Children.value.append(front)
 
   back = loader.create_geometry_from_file(
     "back"
@@ -212,7 +98,7 @@ def start():
     ,"data/materials/Hans.gmd"
     ,avango.gua.LoaderFlags.LOAD_MATERIALS | avango.gua.LoaderFlags.MAKE_PICKABLE
   )
-  rotate_node.Children.value.append(back)
+  Scene.Children.value.append(back)
 
   backdoor = loader.create_geometry_from_file(
     "backdoor"
@@ -220,7 +106,7 @@ def start():
     ,"data/materials/Hans.gmd"
     ,avango.gua.LoaderFlags.LOAD_MATERIALS | avango.gua.LoaderFlags.MAKE_PICKABLE
   )
-  rotate_node.Children.value.append(backdoor)
+  Scene.Children.value.append(backdoor)
 
   left_door = loader.create_geometry_from_file(
     "left_door"
@@ -228,7 +114,7 @@ def start():
     ,"data/materials/Hans.gmd"
     ,avango.gua.LoaderFlags.LOAD_MATERIALS | avango.gua.LoaderFlags.MAKE_PICKABLE
   )
-  rotate_node.Children.value.append(left_door)
+  Scene.Children.value.append(left_door)
 
   # right_door = loader.create_geometry_from_file(
   #   "right_door"
@@ -236,7 +122,7 @@ def start():
   #   ,"data/materials/Hans.gmd"
   #   ,avango.gua.LoaderFlags.LOAD_MATERIALS | avango.gua.LoaderFlags.MAKE_PICKABLE
   # )
-  # rotate_node.Children.value.append(right_door)
+  # Scene.Children.value.append(right_door)
 
   # dashboard = loader.create_geometry_from_file(
   #   "dashboard"
@@ -244,7 +130,7 @@ def start():
   #   ,"data/materials/Hans.gmd"
   #   ,avango.gua.LoaderFlags.LOAD_MATERIALS | avango.gua.LoaderFlags.MAKE_PICKABLE
   # )
-  # rotate_node.Children.value.append(dashboard)
+  # Scene.Children.value.append(dashboard)
 
   hood = loader.create_geometry_from_file(
     "hood"
@@ -252,7 +138,7 @@ def start():
     ,"data/materials/Hans.gmd"
     ,avango.gua.LoaderFlags.LOAD_MATERIALS | avango.gua.LoaderFlags.MAKE_PICKABLE
   )
-  rotate_node.Children.value.append(hood)
+  Scene.Children.value.append(hood)
 
   driver_seat = loader.create_geometry_from_file(
     "driver_seat"
@@ -260,7 +146,7 @@ def start():
     ,"data/materials/Hans.gmd"
     ,avango.gua.LoaderFlags.LOAD_MATERIALS | avango.gua.LoaderFlags.MAKE_PICKABLE
   )
-  rotate_node.Children.value.append(driver_seat)
+  Scene.Children.value.append(driver_seat)
 
   nondriver_seat = loader.create_geometry_from_file(
     "nondriver_seat"
@@ -268,7 +154,7 @@ def start():
     ,"data/materials/Hans.gmd"
     ,avango.gua.LoaderFlags.LOAD_MATERIALS | avango.gua.LoaderFlags.MAKE_PICKABLE
   )
-  rotate_node.Children.value.append(nondriver_seat)
+  Scene.Children.value.append(nondriver_seat)
 
   backseats = loader.create_geometry_from_file(
     "backseats"
@@ -276,7 +162,7 @@ def start():
     ,"data/materials/Hans.gmd"
     ,avango.gua.LoaderFlags.LOAD_MATERIALS | avango.gua.LoaderFlags.MAKE_PICKABLE
   )
-  rotate_node.Children.value.append(backseats)
+  Scene.Children.value.append(backseats)
 
   left_back_tire = loader.create_geometry_from_file(
     "left_back_tire"
@@ -284,7 +170,7 @@ def start():
     ,"data/materials/Hans.gmd"
     ,avango.gua.LoaderFlags.LOAD_MATERIALS | avango.gua.LoaderFlags.MAKE_PICKABLE
   )
-  rotate_node.Children.value.append(left_back_tire)
+  Scene.Children.value.append(left_back_tire)
 
   right_back_tire = loader.create_geometry_from_file(
     "right_back_tire"
@@ -292,7 +178,7 @@ def start():
     ,"data/materials/Hans.gmd"
     ,avango.gua.LoaderFlags.LOAD_MATERIALS | avango.gua.LoaderFlags.MAKE_PICKABLE
   )
-  rotate_node.Children.value.append(right_back_tire)
+  Scene.Children.value.append(right_back_tire)
 
   left_front_tire = loader.create_geometry_from_file(
     "left_front_tire"
@@ -300,7 +186,7 @@ def start():
     ,"data/materials/Hans.gmd"
     ,avango.gua.LoaderFlags.LOAD_MATERIALS | avango.gua.LoaderFlags.MAKE_PICKABLE
   )
-  rotate_node.Children.value.append(left_front_tire)
+  Scene.Children.value.append(left_front_tire)
 
   right_front_tire = loader.create_geometry_from_file(
     "right_front_tire"
@@ -308,7 +194,7 @@ def start():
     ,"data/materials/Hans.gmd"
     ,avango.gua.LoaderFlags.LOAD_MATERIALS | avango.gua.LoaderFlags.MAKE_PICKABLE
   )
-  rotate_node.Children.value.append(right_front_tire)
+  Scene.Children.value.append(right_front_tire)
 
   underbody = loader.create_geometry_from_file(
     "underbody"
@@ -316,7 +202,7 @@ def start():
     ,"data/materials/Hans.gmd"
     ,avango.gua.LoaderFlags.LOAD_MATERIALS | avango.gua.LoaderFlags.MAKE_PICKABLE
   )
-  rotate_node.Children.value.append(underbody)
+  Scene.Children.value.append(underbody)
 
   rest = loader.create_geometry_from_file(
     "rest"
@@ -324,42 +210,15 @@ def start():
     ,"data/materials/Hans.gmd"
     ,avango.gua.LoaderFlags.LOAD_MATERIALS | avango.gua.LoaderFlags.MAKE_PICKABLE
   )
-  rotate_node.Children.value.append(rest)
+  Scene.Children.value.append(rest)
 
 
   Scene.Transform.value = avango.gua.make_trans_mat(0, -6, -20) \
                         * avango.gua.make_rot_mat(-90, 1, 0 ,0) \
-                        * avango.gua.make_scale_mat(0.15)
+                        * avango.gua.make_scale_mat(0.15) 
                         # * avango.gua.make_rot_mat(-120, 0,0,1) \
 
-
-  rotator = TimedRotate()
-  timer = avango.nodes.TimeSensor()
-  rotator.TimeIn.connect_from(timer.Time)
-  rotate_node.Transform.connect_from(rotator.MatrixOut)
-
-  Scene.Children.value.append(rotate_node)
   graph.Root.value.Children.value.append(Scene)
-
-  # # # OLD FASHION CITY
-  # Scene = loader.create_geometry_from_file(
-  #   "Scene_root"
-  #   ,"/opt/3d_models/architecture/old fashion town/old town block.obj"
-  #   ,"data/materials/Grey.gmd"
-  #   ,avango.gua.LoaderFlags.LOAD_MATERIALS or avango.gua.LoaderFlags.NORMALIZE_SCALE
-  # )
-  # Scene.Transform.value = avango.gua.make_trans_mat(0, 0.0, -6) * avango.gua.make_scale_mat(0.015)
-  # graph.Root.value.Children.value.append(Scene)
-
-  # # WEIMARER STADT MODELL
-  # Scene = loader.create_geometry_from_file(
-  #   "Scene_root"
-  #   ,"/opt/3d_models/architecture/weimar_geometry/weimar_stadtmodell_latest_version/weimar_stadtmodell_final.obj"
-  #   ,"data/materials/Grey.gmd"
-  #   ,avango.gua.LoaderFlags.LOAD_MATERIALS or avango.gua.LoaderFlags.NORMALIZE_SCALE
-  # )
-  # Scene.Transform.value = avango.gua.make_trans_mat(0, 0, -6) * avango.gua.make_scale_mat(0.02)
-  # graph.Root.value.Children.value.append(Scene)
 
   sun = avango.gua.nodes.SunLightNode(
     Name = "sun",
@@ -367,11 +226,13 @@ def start():
     Transform = avango.gua.make_rot_mat(-80, 1, 0, 0)
   )
 
-  graph.Root.value.Children.value.append(sun)
+  Scene.Children.value.append(sun)
+
 
   ##create Powerwall setup
   powerwall = LargePowerWall(graph.Root.value)
   avango.gua.create_texture("data/textures/skymap.jpg")
+
   # for target_name in ["tracking-dlp-glasses-{0}".format(i) for i in [1]]:
   for target_name in ["tracking-dlp-glasses-{0}".format(i) for i in [1, 4, 5, 6]]:
     head, pipe = powerwall.create_user(graph)
@@ -391,7 +252,7 @@ def start():
     head.Transform.connect_from(headtracking.MatrixOut)
 
 
-  # create Cone Tree -----------------------------------
+  # create Cone Tree and put it on top of the spheron ------------------------------
   conetree = ConeTree()
   conetree.myConstructor(Scene)
 
@@ -400,41 +261,35 @@ def start():
 
   CT = avango.gua.nodes.TransformNode( Name = "CT")
   CT.Children.value = [CT_root]
-  CT.Transform.value = avango.gua.make_trans_mat(0,0.55,0) * avango.gua.make_scale_mat(0.5)
-  CubeProp = avango.gua.nodes.TransformNode( Name = "CubeProp")
+  CT.Transform.value = avango.gua.make_trans_mat(0,0.55,0)
+  Spheron = avango.gua.nodes.TransformNode( Name = "Spheron")
   CubeTracker = TrackingReader()
   CubeTracker.set_target_name('tracking-new-spheron')
-  CubeProp.Transform.connect_from(CubeTracker.MatrixOut)
+  Spheron.Transform.connect_from(CubeTracker.MatrixOut)
 
-  # # reference--------------------------
-  # reference_cubes = []
-  # for i in range(4):
-  #   reference_cubes.append( loader.create_geometry_from_file(
-  #     "reference_cube",
-  #     "data/objects/cube.obj",
-  #     "data/materials/Red.gmd",
-  #     avango.gua.LoaderFlags.DEFAULTS,
-  #   ))
+  graph.Root.value.Children.value.append(Spheron)
+  Spheron.Children.value.append(CT)
 
-  # reference_cubes[0].Transform.value = avango.gua.make_trans_mat(-0.25, 0.0 ,0.0) * avango.gua.make_scale_mat(0.1)
-  # reference_cubes[1].Transform.value = avango.gua.make_trans_mat( 0.25, 0.0 ,0.0) * avango.gua.make_scale_mat(0.1)
-  # reference_cubes[2].Transform.value = avango.gua.make_trans_mat(-0.25, 0.5 ,0.0) * avango.gua.make_scale_mat(0.1)
-  # reference_cubes[3].Transform.value = avango.gua.make_trans_mat( 0.25, 0.5 ,0.0) * avango.gua.make_scale_mat(0.1)
-  # CubeProp.Children.value.append(reference_cubes[0])
-  # CubeProp.Children.value.append(reference_cubes[1])
-  # CubeProp.Children.value.append(reference_cubes[2])
-  # CubeProp.Children.value.append(reference_cubes[3])
-  # #-----------------------
+  # pickray 
+  pick_ray = avango.gua.nodes.RayNode(Name = "pick_ray")
 
-  graph.Root.value.Children.value.append(CubeProp)
-  CubeProp.Children.value.append(CT)
+  PointerProp = avango.gua.nodes.TransformNode( Name = "PointerProp")
+  PointerTracker = TrackingReader()
+  PointerTracker.set_target_name("tracking-dlp-pointer1")
+  PointerProp.Transform.connect_from(PointerTracker.MatrixOut)
 
+  PointerProp.Children.value.append(pick_ray)
+  graph.Root.value.Children.value.append(PointerProp)
 
+  # initializing the Pick Controller, Pointer Controller, Bounding Box Updater and Test Updater -----
   conetree_picker = PickController()
   conetree_picker.myConstructor(conetree, avango.gua.Vec3(0.15, 0.15, 0.25))
+  conetree_picker.PickedSceneGraph.value = graph
+  conetree_picker.Ray.value = pick_ray
 
   contree_pointer = PointerController()
   contree_pointer.myConstructor(conetree)
+  pick_ray.Transform.connect_from(contree_pointer.RayTransformOut)
 
   BBUpdater = BoundingBoxController()
   BBUpdater.FocusNode.connect_from(conetree.FocusSceneNode)
@@ -445,32 +300,6 @@ def start():
   TextUpdater.TextNode.Transform.value = avango.gua.make_trans_mat(-1.8, 1.1, 0) * avango.gua.make_scale_mat(0.05)
   powerwall.screen.Children.value.append(TextUpdater.TextNode)
 
-  # Light for the Cone Tree
-  sun = avango.gua.nodes.SunLightNode(
-    Name = "sun",
-    Color = avango.gua.Color(1, 1, 1),
-    Transform = avango.gua.make_rot_mat(-80, 1, 0, 0)
-  )
-
-  graph.Root.value.Children.value.append(sun)
-
-
-
-
-  # PICKING
-  pick_ray = avango.gua.nodes.RayNode(Name = "pick_ray")
-  pick_ray.Transform.connect_from(contree_pointer.RayTransformOut)
-
-  PointerProp = avango.gua.nodes.TransformNode( Name = "PointerProp")
-  PointerTracker = TrackingReader()
-  PointerTracker.set_target_name("tracking-dlp-pointer1")
-  PointerProp.Transform.connect_from(PointerTracker.MatrixOut)
-
-  PointerProp.Children.value.append(pick_ray)
-  graph.Root.value.Children.value.append(PointerProp)
-
-  conetree_picker.PickedSceneGraph.value = graph
-  conetree_picker.Ray.value = pick_ray
 
   guaVE = GuaVE()
   guaVE.start(locals(), globals())
